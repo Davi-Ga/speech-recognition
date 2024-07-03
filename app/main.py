4f5daa5fb54e26336ba94d0ec78aa6fa76911ce5
Davi-Ga <contato@daviga.dev.br>
2024-07-03 07:49:18 -0300

    refactor: Update main.py to use variable for audio file name

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ load_dotenv()
 SUBSCRIPTION_KEY = os.getenv('SUBSCRIPTION_KEY')
 SERVICE_REGION = os.getenv('SERVICE_REGION')
 
-wav_file = "YourAudioFile"
+audio_file = "audio_test.ogg"
 
-Recognizer = SpeechRecognize(SUBSCRIPTION_KEY, SERVICE_REGION, "YourAudioFile")
+Recognizer = SpeechRecognize(SUBSCRIPTION_KEY, SERVICE_REGION, audio_file)
 
