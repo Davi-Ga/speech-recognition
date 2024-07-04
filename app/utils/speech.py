615340553068ce5900b168970492db4eeb1fdebe
Davi-Ga <contato@daviga.dev.br>
2024-07-04 18:49:05 -0300

    refactor: Update main.py to use variable for audio file name

--- app/utils/speech.py
+++ app/utils/speech.py
@@ -1,173 +1,119 @@
+import difflib
 import json
 import string
 import time
-import threading
-import wave
-try:
-    import azure.cognitiveservices.speech as speechsdk
-except ImportError:
-    print("""
-    Importing the Speech SDK for Python failed.
-    Refer to
-    https://docs.microsoft.com/azure/cognitive-services/speech-service/quickstart-python for
-    installation instructions.
-    """)
-    import sys
-    sys.exit(1)
+import azure.cognitiveservices.speech as speechsdk
 
+class PronunciationAssessment:
+    def __init__(self, subscription_key, service_region, file,text):
+        self.subscription_key = subscription_key
+        self.service_region = service_region
+        self.file = file or "audio/whatstheweatherlike.wav"
+        self.speech_config = speechsdk.SpeechConfig(subscription=self.subscription_key, region=self.service_region)
+        self.audio_config = speechsdk.audio.AudioConfig(filename=self.file)
+        self.reference_text = text or "What is the weather like."
+        self.enable_miscue = True
+        self.enable_prosody_assessment = True
+        self.pronunciation_config = speechsdk.PronunciationAssessmentConfig(
+            reference_text=self.reference_text,
+            grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark,
+            granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme,
+            enable_miscue=self.enable_miscue)
+        if self.enable_prosody_assessment:
+            self.pronunciation_config.enable_prosody_assessment()
+        self.language = 'en-US'
+        self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config, language=self.language, audio_config=self.audio_config)
+        self.pronunciation_config.apply_to(self.speech_recognizer)
+        self.done = False
+        self.recognized_words = []
+        self.fluency_scores = []
+        self.prosody_scores = []
+        self.durations = []
 
-def pronunciation_assessment_continuous_from_file(subscription_key, service_region, file):
-    """Performs continuous pronunciation assessment asynchronously with input from an audio file.
-        See more information at https://aka.ms/csspeech/pa"""
-
-    import difflib
-    import json
-
-    # Creates an instance of a speech config with specified subscription key and service region.
-    # Replace with your own subscription key and service region (e.g., "westus").
-    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=service_region)
-    # provide a WAV file as an example. Replace it with your own.
-    audio_config = speechsdk.audio.AudioConfig(filename=file)
-    print('audio_config', audio_config)
-
-    # reference_text = "I love my boyfriend. He is literally the love of my life. My little xuxu"
-    reference_text = "whats the weather like"
-    # create pronunciation assessment config, set grading system, granularity and if enable miscue based on your requirement.
-    enable_miscue = True
-    enable_prosody_assessment = True
-    pronunciation_config = speechsdk.PronunciationAssessmentConfig(
-        reference_text=reference_text,
-        grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark,
-        granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme,
-        enable_miscue=enable_miscue)
-    if enable_prosody_assessment:
-        pronunciation_config.enable_prosody_assessment()
-
-    # Creates a speech recognizer using a file as audio input.
-    language = 'en-US'
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language=language, audio_config=audio_config)
-    # apply pronunciation assessment config to speech recognizer
-    pronunciation_config.apply_to(speech_recognizer)
-
-    done = False
-    recognized_words = []
-    fluency_scores = []
-    prosody_scores = []
-    durations = []
-    
-    def canceled(evt: speechsdk.SpeechRecognitionCanceledEventArgs):
-        # Verifica o motivo do cancelamento
+    def canceled(self, evt):
         if evt.reason == speechsdk.CancellationReason.Error:
-            print("Reconhecimento cancelado devido a um erro.")
-            print(f"Detalhes do Erro: {evt.error_details}")
-            print(f"Código do Erro: {evt.error_code}")
+            print("Recognition canceled due to an error.")
+            print(f"Error Details: {evt.error_details}")
+            print(f"Error Code: {evt.error_code}")
         elif evt.reason == speechsdk.CancellationReason.EndOfStream:
-            print("Reconhecimento cancelado porque o fim do stream foi alcançado.")
+            print("Recognition canceled because the end of the stream was reached.")
         else:
-            print("Reconhecimento cancelado por outro motivo.")
+            print("Recognition canceled for another reason.")
 
-    def stop_cb(evt: speechsdk.SessionEventArgs):
-        """callback that signals to stop continuous recognition upon receiving an event `evt`"""
+    def stop_cb(self, evt):
         print('CLOSING on {}'.format(evt))
-        nonlocal done
-        done = True
+        self.done = True
 
-    def recognized(evt: speechsdk.SpeechRecognitionEventArgs):
-        print('pronunciation assessment for: {}'.format(evt.result.text))
+    def recognized(self, evt):
+        print('Pronunciation assessment for: {}'.format(evt.result.text))
         pronunciation_result = speechsdk.PronunciationAssessmentResult(evt.result)
         print('    Accuracy score: {}, pronunciation score: {}, completeness score : {}, fluency score: {}, prosody score: {}'.format(
             pronunciation_result.accuracy_score, pronunciation_result.pronunciation_score,
             pronunciation_result.completeness_score, pronunciation_result.fluency_score, pronunciation_result.prosody_score
         ))
-      
-        nonlocal recognized_words, fluency_scores, durations, prosody_scores
-        recognized_words += pronunciation_result.words
-        fluency_scores.append(pronunciation_result.fluency_score)
-        prosody_scores.append(pronunciation_result.prosody_score)
+        self.recognized_words += pronunciation_result.words
+        self.fluency_scores.append(pronunciation_result.fluency_score)
+        self.prosody_scores.append(pronunciation_result.prosody_score)
         json_result = evt.result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
         jo = json.loads(json_result)
         nb = jo['NBest'][0]
-        durations.append(sum([int(w['Duration']) for w in nb['Words']]))
+        self.durations.append(sum([int(w['Duration']) for w in nb['Words']]))
 
-    # Connect callbacks to the events fired by the speech recognizer
-    speech_recognizer.recognized.connect(recognized)
-    speech_recognizer.canceled.connect(canceled)
-    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
-    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
-    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
-    
-    # stop continuous recognition on either session stopped or canceled events
-    speech_recognizer.session_stopped.connect(stop_cb)
-    speech_recognizer.canceled.connect(stop_cb)
+    def connect_callbacks(self):
+        self.speech_recognizer.recognized.connect(self.recognized)
+        self.speech_recognizer.canceled.connect(self.canceled)
+        self.speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+        self.speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+        self.speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+        self.speech_recognizer.session_stopped.connect(self.stop_cb)
+        self.speech_recognizer.canceled.connect(self.stop_cb)
 
-    # Start continuous pronunciation assessment
-    speech_recognizer.start_continuous_recognition()
-    while not done:
-        time.sleep(.5)
+    def start_assessment(self):
+        self.connect_callbacks()
+        self.speech_recognizer.start_continuous_recognition()
+        while not self.done:
+            time.sleep(.5)
+        self.speech_recognizer.stop_continuous_recognition()
+        self.process_results()
 
-    speech_recognizer.stop_continuous_recognition()
+    def process_results(self):
+        reference_words = [w.strip(string.punctuation) for w in self.reference_text.lower().split()]
+        if self.enable_miscue:
+            diff = difflib.SequenceMatcher(None, reference_words, [x.word.lower() for x in self.recognized_words])
+            final_words = []
+            for tag, i1, i2, j1, j2 in diff.get_opcodes():
+                if tag in ['insert', 'replace']:
+                    for word in self.recognized_words[j1:j2]:
+                        if word.error_type == 'None':
+                            word._error_type = 'Insertion'
+                        final_words.append(word)
+                if tag in ['delete', 'replace']:
+                    for word_text in reference_words[i1:i2]:
+                        word = speechsdk.PronunciationAssessmentWordResult({
+                            'Word': word_text,
+                            'PronunciationAssessment': {
+                                'ErrorType': 'Omission',
+                            }
+                        })
+                        final_words.append(word)
+                if tag == 'equal':
+                    final_words += self.recognized_words[j1:j2]
+        else:
+            final_words = self.recognized_words
 
+        final_accuracy_scores = [word.accuracy_score for word in final_words if hasattr(word, 'accuracy_score')]
+        accuracy_score = sum(final_accuracy_scores) / len(final_accuracy_scores) if final_accuracy_scores else 0
+        fluency_score = sum([x * y for (x, y) in zip(self.fluency_scores, self.durations)]) / sum(self.durations) if self.durations else 0
+        completeness_score = len([w for w in final_words if w.error_type == "None"]) / len(reference_words) * 100 if reference_words else 0
+        completeness_score = min(completeness_score, 100)
+        prosody_score = sum(self.prosody_scores) / len(self.prosody_scores) if self.prosody_scores else 0
+        pron_score = accuracy_score * 0.4 + prosody_score * 0.2 + fluency_score * 0.2 + completeness_score * 0.2
 
-    reference_words = [w.strip(string.punctuation) for w in reference_text.lower().split()]
-
-    # For continuous pronunciation assessment mode, the service won't return the words with `Insertion` or `Omission`
-    # even if miscue is enabled.
-    # We need to compare with the reference text after received all recognized words to get these error words.
-    if enable_miscue:
-        diff = difflib.SequenceMatcher(None, reference_words, [x.word.lower() for x in recognized_words])
-        final_words = []
-        for tag, i1, i2, j1, j2 in diff.get_opcodes():
-            if tag in ['insert', 'replace']:
-                for word in recognized_words[j1:j2]:
-                    if word.error_type == 'None':
-                        word._error_type = 'Insertion'
-                    final_words.append(word)
-            if tag in ['delete', 'replace']:
-                for word_text in reference_words[i1:i2]:
-                    word = speechsdk.PronunciationAssessmentWordResult({
-                        'Word': word_text,
-                        'PronunciationAssessment': {
-                            'ErrorType': 'Omission',
-                        }
-                    })
-                    final_words.append(word)
-            if tag == 'equal':
-                final_words += recognized_words[j1:j2]
-    else:
-        final_words = recognized_words
-
-    # We can calculate whole accuracy by averaging
-    final_accuracy_scores = []
-
-
-    if final_accuracy_scores:  # Check if the list is not empty
-        accuracy_score = sum(final_accuracy_scores) / len(final_accuracy_scores)
-    else:
-        accuracy_score = 0  # Default value or handle accordingly
-
-    # Re-calculate fluency score
-    if sum(durations) > 0:  # Ensure the denominator is not zero
-        fluency_score = sum([x * y for (x, y) in zip(fluency_scores, durations)]) / sum(durations)
-    else:
-        fluency_score = 0  # Default value or handle accordingly
-
-    # Calculate whole completeness score
-    completeness_score = len([w for w in recognized_words if w.error_type == "None"]) / len(reference_words) * 100 if reference_words else 0
-    completeness_score = completeness_score if completeness_score <= 100 else 100
-
-    # Re-calculate prosody score
-    if prosody_scores:  # Check if the list is not empty
-        prosody_score = sum(prosody_scores) / len(prosody_scores)
-    else:
-        prosody_score = 0  # Default value or handle accordingly
-        
-    pron_score = accuracy_score * 0.4 + prosody_score * 0.2 + fluency_score * 0.2 + completeness_score * 0.2
-        
-    print('    Paragraph pronunciation score: {}, accuracy score: {}, completeness score: {}, fluency score: {}, prosody score: {}'.format(
-        pron_score, accuracy_score, completeness_score, fluency_score, prosody_score
-    ))
-
-    for idx, word in enumerate(final_words):
-        print('    {}: word: {}\taccuracy score: {}\terror type: {};'.format(
-            idx + 1, word.word, word.accuracy_score, word.error_type
+        print('    Paragraph pronunciation score: {}, accuracy score: {}, completeness score: {}, fluency score: {}, prosody score: {}'.format(
+            pron_score, accuracy_score, completeness_score, fluency_score, prosody_score
         ))
+
+        for idx, word in enumerate(final_words):
+            print('    {}: word: {}\taccuracy score: {}\terror type: {};'.format(
+                idx + 1, word.word, word.accuracy_score, word.error_type
+            ))
